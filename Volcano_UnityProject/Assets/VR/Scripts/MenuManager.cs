6dd77aef55f0153efee4606b417b3724b5a9929c
steveulrich <steveulrich@gmail.com>
2015-09-14 17:05:17 -0700

    Removed trail signs in favor of a menu below the user. 3D Cursor implemented, need to implement timer visualization

---
+++ Volcano_UnityProject/Assets/VR/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Transform menuCanvas;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        menuCanvas.position = transform.position + (Vector3.down * 1.5F);
+	}
+}
---
+++ Volcano_UnityProject/Assets/VR/Scripts/TrailSigns.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailSigns : MonoBehaviour {
+
+    public GameObject[] trailSignsFwd;
+    public GameObject[] trailSignsBwd;
+}
