d5ff4efda72d08ee34a13a6d8efac6e40f9932fc
djdevvydev <devon.copley@magneticwest.net>
2015-09-28 21:29:26 -0700

    New Credits scene
    Credits are sort of working. Video Sphere is included but disabled.

---
+++ Volcano_UnityProject/Assets/Art_Animation/Credits Animation Script.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+function Awake ()
+{
+    Debug.Log("Awake called.");
+}
+
+
+function Start ()
+{
+	var credits1 : GameObject;
+	var credits2 : GameObject;
+	var credits3 : GameObject;
+    var anim: Animation;
+
+	credits1 = GameObject.Find("Credits_Plane_1");
+	credits2 = GameObject.Find("Credits_Plane_2");
+	credits3 = GameObject.Find("Credits_Plane_3");
+
+    Debug.Log("Start called.");
+    yield WaitForSeconds(4);
+    Debug.Log("calling Animation.");
+    anim=credits2.GetComponent.<Animation>();
+    anim.Play();
+
+    yield WaitForSeconds(4);
+    Debug.Log("calling Animation.");
+    anim=credits3.GetComponent.<Animation>();
+    anim.Play();
+    
+}
